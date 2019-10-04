65fd2f10cd6cbe251c98d4bd13c4dfdb0595899a
Scott Moore <woodwindscott@yahoo.com>
2019-10-03 21:29:08 -0400

    Initial File Structure

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+
+    selectAll: function(XXXXX, cb) {
+
+    },
+    insertOne: function(XXXXX, cb) {
+
+    },
+    updateOne: function(XXXXX, cb) {
+
+    }
+
+}
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Whiskey River Barbecue Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Barbecue Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('All American Burger');
